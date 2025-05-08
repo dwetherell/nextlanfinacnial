6b41cd0b2050e938919ea0b9b3d55bebf69d9905
David Wetherell <dvdwetherell@gmail.com>
2025-05-08 11:58:54 -0400

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+import '../scss/bootstrap.scss';
+import '../scss/icons.scss';
+import '../scss/app.scss';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
